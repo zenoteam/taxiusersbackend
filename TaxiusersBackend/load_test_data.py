6af3fa34f2850f5f312dd6466bea45377328071b
Andyphied <philipandy2@gmail.com>
2021-03-16 05:44:15 +0100

    updated load data

--- TaxiusersBackend/load_test_data.py
+++ TaxiusersBackend/load_test_data.py
@@ -9,21 +9,20 @@ if __name__ == '__main__':
     # Create some test data
     test_data = [
         # super admin user
-        ('bruce', "bruce", 1, datetime.now()),
+        ('bruce', "bruce", 1, datetime.now(), "dnd"),
         # admin user
-        ('esther', "esther", 2, datetime.now()),
+        ('esther', "esther", 2, datetime.now(), "hdhd"),
         # driver
-        ('stephen', "stephen", 3, datetime.now()),
+        ('stephen', "stephen", 3, datetime.now(), "ddd"),
         # rider
-        ('jacob', "jacob", 4, datetime.now()),
+        ('jacob', "jacob", 4, datetime.now(), "dddd"),
     ]
-    for username, password, role, createdAt in test_data:
-        user = UserModel(
-            username=username,
-            password=password,
-            role=role,
-            createdAt=createdAt
-        )
+    for username, password, role, createdAt, firebaseToken in test_data:
+        user = UserModel(username=username,
+                         password=password,
+                         role=role,
+                         createdAt=createdAt,
+                         firebaseToken=firebaseToken)
         application.db.session.add(user)
 
     application.db.session.commit()
